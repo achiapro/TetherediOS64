5ed3e2302a50ddc219aff9cfe56aafe18a9a776d
Exploite-d <51859413+Exploite-d@users.noreply.github.com>
2020-02-05 23:35:29 +0100

    Add files via upload

---
+++ files/tether_boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo "***Ch3rryflower64 Booter***"
+echo "All Thanks to @tihmstar here for ra1nsn0w"
+echo "Make sure you have have apnonce.shsh2 in this this folder and boot.ipsw(original ipsw from version you downgraded to) ( it can be any blob from tsssaver for example)
+
+./ra1nsn0w -t apnonce.shsh2 boot.ipsw -b "-v" 
+
+
