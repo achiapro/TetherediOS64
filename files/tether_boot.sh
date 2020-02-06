37e9af5a7067d04f6c6151b37b9eb1362b1edd4c
Exploite-d <51859413+Exploite-d@users.noreply.github.com>
2020-02-06 14:41:05 +0100

    Update tether_boot.sh

--- files/tether_boot.sh
+++ files/tether_boot.sh
@@ -3,8 +3,10 @@ clear
 echo "***Ch3rryflower64 Booter***"
 echo "All Thanks to @tihmstar here for ra1nsn0w"
 echo "Make sure you have have apnonce.shsh2 in this this folder and boot.ipsw(original ipsw from version you downgraded to) ( it can be any blob from tsssaver for example)"
+cd ipwndfu_public
 ./ipwndfu -p
 python rmsigchks.py
+cd ../
 ./ra1nsn0w -t apnonce.shsh2 boot.ipsw -b "-v" 
 
 
